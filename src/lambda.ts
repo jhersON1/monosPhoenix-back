01f7a5cf45de21febd85adf16ffe648912ba8634
MarioQuimetMed <mario.medina@ficct.uagrm.edu.bo>
2026-07-15 23:31:48 -0400

    fix/ID-19 update lambda.ts

--- src/lambda.ts
+++ src/lambda.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { Handler, Context } from 'aws-lambda';
+import 'pg';
 
 let cachedServer: Handler;
 

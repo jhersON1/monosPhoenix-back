c2e6e539c30c41c53cc11c677e96213057b1d3b5
MarioQuimetMed <mario.medina@ficct.uagrm.edu.bo>
2026-07-15 22:20:42 -0400

    fix/ID-19 changes to webpack-config and config.yml

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,23 @@
-module.exports = {
-  externals: {
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = function (options, webpack) {
+  return {
+    ...options,
+      externals: {
     pg: 'commonjs pg',
     typeorm: 'commonjs typeorm',
   },
+    optimization: {
+      ...options.optimization,
+    //   minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          terserOptions: {
+            keep_classnames: true,
+            keep_fnames: true,
+          },
+        }),
+      ],
+    },
+  };
 };

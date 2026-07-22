2836fe90edda8efefe49397515936b65548f84fb
jherson <rodriguezpaul98@gmail.com>
2026-07-21 23:49:20 -0400

    fix(circleci): improve and add security

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,19 +7,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        type: 'postgres',
-        host: config.get<string>('DB_HOST'),
-        port: config.get<number>('DB_PORT'),
-        username: config.get<string>('DB_USER'),
-        password: config.get<string>('DB_PASSWORD'),
-        database: config.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }),
+      useFactory: (config: ConfigService) => {
+        const sslEnabled = config.get<string>('DB_SSL', 'true').toLowerCase() === 'true';
+
+        return {
+          type: 'postgres',
+          host: config.get<string>('DB_HOST'),
+          port: config.get<number>('DB_PORT'),
+          username: config.get<string>('DB_USER'),
+          password: config.get<string>('DB_PASSWORD'),
+          database: config.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+          ssl: sslEnabled ? { rejectUnauthorized: false } : false,
+        };
+      },
     }),
   ],
 })

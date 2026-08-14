fd1a8d5c2c16887304d9a053fc1f5c1614f64bd7
MarioQuimetMed <mario.medina@ficct.uagrm.edu.bo>
2026-08-13 22:58:07 -0400

    feature/update to main.ts

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, documentFactory);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 
 void bootstrap();
